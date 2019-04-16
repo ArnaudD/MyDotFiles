c2c7b5c6e6f110aed090b6fe3e0b97e415593de2
Arnaud Didry <arnaud@fizix.io>
2019-04-16 12:24:28 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-ls -a MyDotFiles | grep -vP "install|config|\.gitignore|\.gitmodules|\.git|\.\.|\.$" | xargs -I '{}' ln -nfsv  `pwd`'/MyDotFiles/{}' {}
-ls -a MyDotFiles/config | grep -vP "\.\.|\.$" | xargs -I '{}' ln -nfsv  `pwd`'/MyDotFiles/config/{}' .config/{}
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ls -a $CURRENT_DIR | grep -vP "atom|install|config|\.gitignore|\.gitmodules|\.git|.ssh|\.\.|\.$" | xargs -I '{}' ln -nfsv  $CURRENT_DIR'/{}' ~/{}
+ls -a $CURRENT_DIR/config | grep -vP "\.\.|\.$" | xargs -I '{}' ln -nfsv  $CURRENT_DIR'/config/{}' ~/.config/{}
+ls -a $CURRENT_DIR/.ssh | grep -vP "\.\.|\.$" | xargs -I '{}' ln -nfsv  $CURRENT_DIR'/.ssh/{}' ~/.ssh/{}
+ls -a $CURRENT_DIR/.atom | grep -vP "\.\.|\.$" | xargs -I '{}' ln -nfsv  $CURRENT_DIR'/.atom/{}' ~/.atom/{}
+ln -nfsv  $CURRENT_DIR/.gitconfig ~/.gitconfig
